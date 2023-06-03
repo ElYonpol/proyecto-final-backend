9e550f5a7d2eeb3e0a89530560da54d0337af41e
Juan Pablo Portas Esquivel <jppe@yahoo.com.ar>
2023-06-03 13:05:40 -0300

    Entrega

--- src/config/config.js
+++ src/config/config.js
@@ -15,6 +15,9 @@ const objConfig = {
 	persistence: process.env.PERSISTENCE,
 	gmail_mail_user: process.env.GMAIL_MAIL_USER,
 	gmail_pass: process.env.GOOGLE_APPLICATION_PASSWORD,
+	twilio_account_sid: process.env.TWILIO_ACCOUNT_SID,
+	twilio_auth_token: process.env.TWILIO_AUTH_TOKEN,
+	twilio_phone_number: process.env.TWILIO_PHONE_NUMBER,
 	dbConnection: async () => MongoSingleton.getInstance(),
 	
 	// connectDB: async () => {
