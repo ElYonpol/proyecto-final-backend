0579dff100fa780f6a77dab609552ccda5881602
Juan Pablo Portas Esquivel <jppe@yahoo.com.ar>
2023-06-11 18:45:26 -0300

    Entrega

--- src/routes/sessionRouter.js
+++ src/routes/sessionRouter.js
@@ -13,12 +13,12 @@ const { objectsValidation } = require("../middleware/validator.js");
 
 const sessionsRouter = Router();
 
-// GET http://localhost:8080/sessions/login
+// GET http://localhost:8080/api/sessions/login
 sessionsRouter.get("/login", (req, res) => {
 	res.render("login");
 });
 
-// POST http://localhost:8080/sessions/login
+// POST http://localhost:8080/api/sessions/login
 sessionsRouter.post(
 	"/login",
 	objectsValidation(usersLoginSchema),
@@ -61,11 +61,10 @@ sessionsRouter.post(
 	}
 );
 
-// GET http://localhost:8080/sessions/current
+// GET http://localhost:8080/api/sessions/current
 sessionsRouter.get(
 	"/current",
-	authPassport("jwt"),
-	authRole("user"),
+	authToken,
 	(req, res) => {
 		res.send({
 			status: "success",
@@ -74,12 +73,12 @@ sessionsRouter.get(
 	}
 );
 
-// GET http://localhost:8080/sessions/register
+// GET http://localhost:8080/api/sessions/register
 sessionsRouter.get("/register", (req, res) => {
 	res.render("register");
 });
 
-// POST http://localhost:8080/sessions/register
+// POST http://localhost:8080/api/sessions/register
 sessionsRouter.post(
 	"/register",
 	objectsValidation(usersRegisterSchema),
@@ -129,7 +128,7 @@ sessionsRouter.post(
 	}
 );
 
-// GET http://localhost:8080/sessions/github
+// GET http://localhost:8080/api/sessions/github
 sessionsRouter.get("/github", passport.authenticate("github"));
 
 sessionsRouter.get(
@@ -144,17 +143,17 @@ sessionsRouter.get(
 	}
 );
 
-// GET http://localhost:8080/sessions/failregister
+// GET http://localhost:8080/api/sessions/failregister
 sessionsRouter.get("/failregister", (req, res) => {
 	res.send({ status: "error", message: "Error al crear el usuario" });
 });
 
-// GET http://localhost:8080/sessions/faillogin
+// GET http://localhost:8080/api/sessions/faillogin
 sessionsRouter.get("/faillogin", (req, res) => {
 	res.send({ status: "error", message: "Error al realizar el login" });
 });
 
-// PUT http://localhost:8080/sessions/recoverypass
+// PUT http://localhost:8080/api/sessions/recoverypass
 sessionsRouter.put("/recoverypass", async (req, res) => {
 	const { email, password } = req.body;
 	const user = await userService.getByEmail(email);
@@ -167,7 +166,7 @@ sessionsRouter.put("/recoverypass", async (req, res) => {
 	res.send({ status: "success", message: "Contraseña actualizada" });
 });
 
-// GET http://localhost:8080/sessions/logout
+// GET http://localhost:8080/api/sessions/logout
 sessionsRouter.get("/logout", (req, res) => {
 	req.session.destroy((err) => {
 		if (err) return res.send({ status: "Logout error", message: err });
