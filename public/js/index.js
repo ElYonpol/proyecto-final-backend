94e4e6bf97a97da43ff3bdc5e7a10a7962196125
Juan Pablo Portas Esquivel <jppe@yahoo.com.ar>
2023-08-27 18:01:10 -0300

    Entrega

--- public/js/index.js
+++ public/js/index.js
@@ -8,12 +8,24 @@ const showCartIcon = async () => {
 			headers: { "content-type": "application/json" },
 		});
 
-		let data = await response.json();
+		if (response.status === 404) {
+			Swal.fire({
+				icon: "info",
+				title: "Tip",
+				text: "Para ingresar selecciona la opción LOGIN del menú",
+				toast: true,
+				position: "top-right",
+				showConfirmButton: false,
+				timer: 1500,
+			});
+		} else {
+			let data = await response.json();
 
-		if (data.status === "success") {
-			let cid = data.payload.cart;
-			userCart.href = `/carts/${cid}`;
-			userCart.style.display = "";
+			if (data.status === "success") {
+				let cid = data.payload.cart;
+				userCart.href = `/carts/${cid}`;
+				userCart.style.display = "";
+			}
 		}
 	} catch (error) {
 		Swal.fire({
@@ -24,4 +36,4 @@ const showCartIcon = async () => {
 	}
 };
 
-showCartIcon()
+showCartIcon();
